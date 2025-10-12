9ee52227297e95f47a49ebfbeca0967b7bbf119f
bryanmsombra27 <bryanjm96@gmail.com>
2025-10-11 20:55:28 -0600

    socket implementation

--- src/pages/admin/home/Home.tsx
+++ src/pages/admin/home/Home.tsx
@@ -1,7 +1,25 @@
-import type { FC } from "react";
+import { socket } from "@/config/socket";
+import { useEffect, type FC } from "react";
 
 interface HomeProps {}
 const Home: FC<HomeProps> = ({}) => {
+  useEffect(() => {
+    const handleTest = (value: any) => {
+      console.log(value, "valor de backend");
+    };
+
+    if (socket) {
+      socket.on("test", handleTest);
+      socket.emit("test", {
+        text: "vale verga",
+      });
+    }
+
+    return () => {
+      socket.off("test", handleTest);
+    };
+  }, [socket]);
+
   return <h1>Component</h1>;
 };
 
